3a8cb5bfba214b9119f712f564f31e5d2074ba58
Anvi Alex <epononanvialex@gmail.com>
2021-04-13 20:53:39 +0300

    self join and join answers

--- queries.sql
+++ queries.sql
@@ -313,3 +313,129 @@ SELECT DISTINCT name
 FROM actor JOIN casting ON id=actorid
 WHERE movieid IN (SELECT movieid FROM casting JOIN actor ON (actorid=id AND name='Art Garfunkel')) AND name != 'Art Garfunkel'
 GROUP BY name
+
+-- Note the INNER JOIN misses the teacher with no department and the department with no teacher
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+--Use a different JOIN so that all teachers are listed.
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+          ON (teacher.dept=dept.id)
+
+--Use a different JOIN so that all departments are listed.
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept
+          ON (teacher.dept=dept.id)
+
+--Use COALESCE to print the mobile number. Use the number '07986 444 2266' there is no number given. Show teacher name and mobile number or '07986 444 2266'
+SELECT name,
+COALESCE(mobile, '07986 444 2266')
+FROM teacher
+
+--Use the COALESCE function and a LEFT JOIN to print the name and department name. Use the string 'None' where there is no department.
+SELECT COALESCE(teacher.name, 'NONE'), COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+--Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+--Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+GROUP BY dept.name
+
+--Use CASE to show the name of each teacher followed by 'Sci' if the the teacher is in dept 1 or 2 and 'Art' otherwise.
+
+SELECT teacher.name,
+CASE WHEN dept.id = 1 THEN 'Sci'
+     WHEN dept.id = 2 THEN 'Sci'
+     ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+--Use CASE to show the name of each teacher followed by 'Sci' if the the teacher is in dept 1 or 2 show 'Art' if the dept is 3 and 'None' otherwise.
+SELECT teacher.name,
+CASE
+WHEN dept.id = 1 THEN 'Sci'
+WHEN dept.id = 2 THEN 'Sci'
+WHEN dept.id = 3 THEN 'Art'
+ELSE 'None' END
+FROM teacher LEFT JOIN dept ON (dept.id=teacher.dept)
+
+--How many stops are in the database.
+SELECT COUNT(*)
+FROM stops
+
+--Find the id value for the stop 'Craiglockhart'
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart'
+
+--Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT id, name
+FROM stops
+    JOIN route ON id=stop
+WHERE company = 'LRT' AND num=4
+
+--The query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2.
+-- Add a HAVING clause to restrict the output to these two routes
+SELECT company, num, COUNT(*) AS visits
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING visits=2
+
+--Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
+
+--The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown.
+--If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'AND stopb.name = 'London Road'
+
+--Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company =b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Haymarket' AND stopb.name='Leith'
+
+--Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+SELECT DISTINCT a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+
+--Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself. Include the company and bus no. of the relevant services.
+SELECT stopa.name, a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopb.name = 'Craiglockhart'
+
+--Find the routes involving two buses that can go from Craiglockhart to Sighthill.
+-- Show the bus no. and company for the first bus, the name of the stop for the transfer,
+-- and the bus no. and company for the second bus.
+SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
+            AND  stopb.name = stopc.name
+ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
