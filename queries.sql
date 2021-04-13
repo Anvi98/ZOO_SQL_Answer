3439618ced501f2f077069ec6de0940b2e971bc8
Anvi Alex <epononanvialex@gmail.com>
2021-04-13 19:06:44 +0300

    join questions

--- queries.sql
+++ queries.sql
@@ -130,3 +130,104 @@ ON y.continent = x.continent
 -- Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
 SELECT name, continent FROM world x
   WHERE population > ALL(SELECT 3*population FROM world y WHERE x.continent = y.continent AND x.name <> y.name)
+
+--Show the total population of the world.
+SELECT SUM(population)
+FROM world
+
+-- List all the continents - just once each.
+SELECT DISTINCT continent FROM world;
+
+-- Give the total GDP of Africa
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa'
+
+--How many countries have an area of at least 1000000
+SELECT COUNT(name) FROM world WHERE area >= 1000000;
+
+-- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+-- For each continent show the continent and number of countries.
+SELECT continent, COUNT(name) FROM world GROUP BY continent;
+
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT continent, COUNT(name) from world WHERE population >= 10000000 GROUP BY continent;
+
+-- List the continents that have a total population of at least 100 million.
+SELECT continent FROM world GROUP BY continent HAVING  SUM(population)> 100000000;
+
+--Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER'
+
+-- From the previous query you can see that Lars Bender's scored a goal in game 1012. Now we want to know what teams were playing in that match.
+-- Notice in the that the column matchid in the goal table corresponds to the id column in the game table. We can look up information about game 1012 by finding that row in the game table.
+-- Show id, stadium, team1, team2 for just game 1012
+SELECT id,stadium,team1,team2
+  FROM game WHERE id ='1012'
+
+-- Modify it to show the player, teamid, stadium and mdate for every German goal.
+  SELECT player,teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+  WHERE teamid = 'GER'
+
+
+-- Use the same JOIN as in the previous question.
+Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT team1, team2, player
+FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
+
+-- Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON (teamid=id)
+ WHERE gtime<=10
+
+-- List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+ SELECT mdate, teamname
+FROM game JOIN eteam ON (team1=eteam.id)
+WHERE coach ='Fernando Santos'
+
+
+-- List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+SELECT player
+FROM goal JOIN game ON (matchid=id)
+WHERE stadium = 'National Stadium, Warsaw';
+
+-- The example query shows all goals scored in the Germany-Greece quarterfinal.
+-- Instead show the name of all players who scored a goal against Germany.
+SELECT DISTINCT(player)
+FROM game
+  JOIN goal ON matchid = id
+WHERE ((team1='GER' OR team2='GER') AND teamid != 'GER')
+
+-- Show teamname and the total number of goals scored.
+SELECT teamname, COUNT(player)
+  FROM eteam JOIN goal ON id=teamid
+ GROUP BY teamname
+
+-- Show the stadium and the number of goals scored in each stadium.
+ SELECT stadium, COUNT(player)
+FROM game JOIN goal ON (id=matchid)
+GROUP BY stadium
+
+-- For every match involving 'POL', show the matchid, date and the number of goals scored.
+SELECT matchid, mdate, COUNT(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid, mdate
+
+-- For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+SELECT matchid, mdate, COUNT(player)
+FROM goal JOIN game ON (matchid=id)
+WHERE (team1 = 'GER' or team2 ='GER') AND teamid ='GER'
+GROUP BY matchid, mdate
+
+-- List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
+SELECT mdate,
+       team1,
+       SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
+       team2,
+       SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2 FROM
+    game LEFT JOIN goal ON (id = matchid)
+    GROUP BY mdate,team1,team2, matchid
